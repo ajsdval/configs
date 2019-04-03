12f85e519ef7e734d3c7a41d76555880627f7b9f
Anthony Sandoval <git@sdval.com>
2019-04-03 15:01:53 -0400

    setting up ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+pipenv="python3 -m pipenv"
 
 darwin () {
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
 linux () {
-    sudo apt install -y make
-    sudo apt install python3
+    sudo apt install -y make python3 python3-pip python3-apt
+    python3 -m pip install --upgrade pip pipenv
+    sudo $pipenv --rm && rm Pipfile Pipfile.lock
+    $pipenv --python 3.6 --site-packages
+    $pipenv install --skip-lock ansible
 }
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     darwin
-elif [[ "$OSTYPE" == "linux+gnu" ]]; then
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     linux
 fi
