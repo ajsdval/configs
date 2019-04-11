5f7bfa2113f4dcffc6498021911a3ea2f4cf584b
Anthony Sandoval <asandoval@gitlab.com>
2019-04-10 19:56:16 -0500

    more logic changes for OSX

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,23 @@
 pipenv="python3 -m pipenv"
 
 darwin () {
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if hash brew 2>/dev/null; then
+        echo "Homebrew is already installed. Skipping "
+    else
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    python3 -m pip install --upgrade pip pipenv
+    pipenv --rm && rm Pipfile Pipfile.lock
+    $pipenv --python 3
+    $pipenv install ansible
 }
 
 linux () {
     sudo apt install -y make python3 python3-pip python3-apt
     python3 -m pip install --upgrade pip pipenv
     sudo $pipenv --rm && rm Pipfile Pipfile.lock
-    $pipenv --python 3.6 --site-packages
+    $pipenv --python 3 --site-packages
     $pipenv install --skip-lock ansible
 }
 
@@ -19,3 +28,5 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     linux
 fi
+
+make
